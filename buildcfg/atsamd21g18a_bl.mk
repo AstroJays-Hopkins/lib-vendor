dc71a815656d070e38bff6b890522df57d8b28bf
Mixih <mxhahn1@gmail.com>
2021-10-08 19:31:22 -0400

    Version 0.4.1
    Added bootloader linkerscript for SAMD21G18a.

---
+++ buildcfg/atsamd21g18a_bl.mk
@@ -0,0 +1,14 @@
+############################
+#   Device Def Makefile    #
+############################
+# Must include:
+# UC_FLAGS - specifies which mcu
+# UC_INCLUDES - where to get headers
+# UC_LIBS - stuff passed to the linker for linking
+# DO NOT OVERWRITE; you should append "+=" instead
+
+DEV      := samd21g18a
+DEV_FLAG := __ATSAMD21G18A__
+SERIES   := a
+
+include $(LIBROOT)/buildcfg/samd21-common_bl.mk 
---
+++ buildcfg/samd21-common_bl.mk
@@ -0,0 +1,15 @@
+#########################################
+# ATDAMD51 Packs Common Make Components #
+#########################################
+DEVKIT := dfp_atsamd21
+
+VENDOR_UC_FLAGS := -mcpu=cortex-m0plus -mthumb -D$(DEV_FLAG)
+VENDOR_CFLAGS   += $(VENDOR_UC_FLAGS)
+VENDOR_INCLUDES += -I$(LIBROOT)/arm-cmsis/include \
+		   -I$(LIBROOT)/$(DEVKIT)/samd21$(SERIES)/include
+VENDOR_LDFLAGS  += $(VENDOR_UC_FLAGS) \
+                   -T$(LIBROOT)/$(DEVKIT)/samd21$(SERIES)/ldscripts/$(DEV)_flash_bl.ld \
+                   $(LIBROOT)/$(DEVKIT)/samd21$(SERIES)/ldscripts/newlib_compat.ld \
+                   -L$(LIBROOT)/$(DEVKIT)/samd21$(SERIES)/lib -L$(LIBROOT)/arm-cmsis/lib
+# whole archive needed to get the exception table to link
+VENDOR_LDLIBS   += -Wl,--whole-archive -l$(DEV) -Wl,--no-whole-archive -lm
